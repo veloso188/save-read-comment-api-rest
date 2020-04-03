4e67f6e53f70cb57d327063c0333ff92f7229be9
Gabriel <veloso188@gmail.com>
2020-04-03 20:01:58 -0300

    remove unused folders
    remove unused folders

--- save-read-comment-api-rest/src/main/java/com/commentapp/controller/CommentsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.commentapp.controller;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentsRepository extends JpaRepository<Comment, Long>{
-
-}
